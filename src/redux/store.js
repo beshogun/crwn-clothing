432e087d1c6003e1461a046fc2c7e3773498e5fe
beshogun <ben.roy@omnitude.tech>
2020-01-15 11:42:19 +0000

    added redux and implemented user reducer and user action to header and app components

---
+++ src/redux/store.js
@@ -0,0 +1,14 @@
+import { createStore, applyMiddleware } from "redux";
+import logger from "redux-logger";
+
+import rootReducer from "./root-reducer";
+
+const middlewares = [logger];
+
+const store = createStore(
+    rootReducer, applyMiddleware(...middlewares) /* preloadedState, */
+ +  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  );
+// const store = createStore(rootReducer, applyMiddleware(...middlewares));
+
+export default store;
