bc6d4c61a30c4067261223ae2e1b0ac309134f4c
beshogun <ben.roy@omnitude.tech>
2020-01-19 12:00:27 +0000

    updating stripe integration and updating our favicon and title

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -1,23 +1,23 @@
-import React from "react";
-import { connect } from "react-redux";
-import MenuItem from "../menu-item/menu-item.component";
-import { createStructuredSelector } from "reselect";
-import { selectDirectorySection } from "../../redux/directory/directory.selectors";
+import React from 'react';
+import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
 
-import "./directory.styles.scss";
+import { selectDirectorySections } from '../../redux/directory/directory.selectors';
 
-const Directory = ({ sections }) => {
-  return (
-    <div className="directory-menu">
-      {sections.map(({ id, ...otherSectionsProps }) => (
-        <MenuItem key={id} {...otherSectionsProps} />
-      ))}
-    </div>
-  );
-};
+import MenuItem from '../menu-item/menu-item.component';
+
+import './directory.styles.scss';
+
+const Directory = ({ sections }) => (
+  <div className='directory-menu'>
+    {sections.map(({ id, ...otherSectionProps }) => (
+      <MenuItem key={id} {...otherSectionProps} />
+    ))}
+  </div>
+);
 
 const mapStateToProps = createStructuredSelector({
-  sections: selectDirectorySection
+  sections: selectDirectorySections
 });
 
 export default connect(mapStateToProps)(Directory);
---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import StripeCheckout from "react-stripe-checkout";
+
+export const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  const publishableKey = "pk_test_d04v2FEUuBNsGyDaBjku42TV00o5AClE8y";
+
+  const onToken = token => {
+    console.log(token);
+    alert("Payment Succsesful");
+  };
+
+  return (
+    <StripeCheckout
+      label="Pay Now"
+      name="CRWN Clothing Ltd"
+      billingAddress
+      shippingAddress
+      image="https://svgshare.com/i/CUz.svg"
+      description={`Yor total is £${price}`}
+      amount={priceForStripe}
+      panelLabel="Pay Now"
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  );
+};
+
+export default StripeCheckoutButton;
